d1d30e40768f60c055391a34aaea0290e4b6dcde
maher-khan <nihartz55five@gmail.com>
2026-02-19 00:36:52 -0500

    ICA1 created

--- Maher-Khan-gas-station/Form1.Designer.cs
+++ Maher-Khan-gas-station/Form1.Designer.cs
@@ -28,55 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            txtName = new Label();
-            txtGallons = new Label();
-            lstBox = new ListBox();
+            lblName = new Label();
+            lblGallons = new Label();
+            lstOutput = new ListBox();
             btnCalc = new Button();
             btnClear = new Button();
             btnExit = new Button();
+            txtCustomerName = new TextBox();
+            txtGallons = new TextBox();
             SuspendLayout();
             // 
-            // txtName
+            // lblName
             // 
-            txtName.AutoSize = true;
-            txtName.Location = new Point(217, 63);
-            txtName.Name = "txtName";
-            txtName.Size = new Size(141, 25);
-            txtName.TabIndex = 0;
-            txtName.Text = "Customer Name";
+            lblName.AutoSize = true;
+            lblName.Location = new Point(217, 63);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(141, 25);
+            lblName.TabIndex = 0;
+            lblName.Text = "Customer Name";
             // 
-            // txtGallons
+            // lblGallons
             // 
-            txtGallons.AutoSize = true;
-            txtGallons.Location = new Point(217, 138);
-            txtGallons.Name = "txtGallons";
-            txtGallons.Size = new Size(70, 25);
-            txtGallons.TabIndex = 1;
-            txtGallons.Text = "Gallons";
-            txtGallons.Click += txtGallons_Click;
+            lblGallons.AutoSize = true;
+            lblGallons.Location = new Point(217, 138);
+            lblGallons.Name = "lblGallons";
+            lblGallons.Size = new Size(70, 25);
+            lblGallons.TabIndex = 1;
+            lblGallons.Text = "Gallons";
+            lblGallons.Click += txtGallons_Click;
             // 
-            // lstBox
+            // lstOutput
             // 
-            lstBox.FormattingEnabled = true;
-            lstBox.Location = new Point(217, 227);
-            lstBox.Name = "lstBox";
-            lstBox.Size = new Size(625, 279);
-            lstBox.TabIndex = 2;
+            lstOutput.FormattingEnabled = true;
+            lstOutput.Location = new Point(217, 202);
+            lstOutput.Name = "lstOutput";
+            lstOutput.Size = new Size(625, 279);
+            lstOutput.TabIndex = 2;
+            lstOutput.SelectedIndexChanged += lstOutput_SelectedIndexChanged;
             // 
             // btnCalc
             // 
-            btnCalc.Location = new Point(217, 570);
+            btnCalc.Location = new Point(195, 543);
             btnCalc.Name = "btnCalc";
-            btnCalc.Size = new Size(112, 34);
+            btnCalc.Size = new Size(156, 107);
             btnCalc.TabIndex = 3;
             btnCalc.Text = "&Calculate";
             btnCalc.UseVisualStyleBackColor = true;
+            btnCalc.Click += btnCalc_Click;
             // 
             // btnClear
             // 
-            btnClear.Location = new Point(473, 570);
+            btnClear.Location = new Point(451, 543);
             btnClear.Name = "btnClear";
-            btnClear.Size = new Size(112, 34);
+            btnClear.Size = new Size(156, 107);
             btnClear.TabIndex = 4;
             btnClear.Text = "C&lear";
             btnClear.UseVisualStyleBackColor = true;
@@ -84,24 +88,41 @@
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(730, 570);
+            btnExit.Location = new Point(708, 543);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(112, 34);
+            btnExit.Size = new Size(156, 107);
             btnExit.TabIndex = 5;
             btnExit.Text = "E&xit";
             btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // txtCustomerName
+            // 
+            txtCustomerName.Location = new Point(393, 63);
+            txtCustomerName.Name = "txtCustomerName";
+            txtCustomerName.Size = new Size(449, 31);
+            txtCustomerName.TabIndex = 6;
+            // 
+            // txtGallons
+            // 
+            txtGallons.Location = new Point(393, 132);
+            txtGallons.Name = "txtGallons";
+            txtGallons.Size = new Size(449, 31);
+            txtGallons.TabIndex = 7;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1236, 671);
+            ClientSize = new Size(1105, 717);
+            Controls.Add(txtGallons);
+            Controls.Add(txtCustomerName);
             Controls.Add(btnExit);
             Controls.Add(btnClear);
             Controls.Add(btnCalc);
-            Controls.Add(lstBox);
-            Controls.Add(txtGallons);
-            Controls.Add(txtName);
+            Controls.Add(lstOutput);
+            Controls.Add(lblGallons);
+            Controls.Add(lblName);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -110,11 +131,13 @@
 
         #endregion
 
-        private Label txtName;
-        private Label txtGallons;
-        private ListBox lstBox;
+        private Label lblName;
+        private Label lblGallons;
+        private ListBox lstOutput;
         private Button btnCalc;
         private Button btnClear;
         private Button btnExit;
+        private TextBox txtCustomerName;
+        private TextBox txtGallons;
     }
 }
--- Maher-Khan-gas-station/Form1.cs
+++ Maher-Khan-gas-station/Form1.cs
@@ -14,6 +14,25 @@ namespace Maher_Khan_gas_station
 
         private void button2_Click(object sender, EventArgs e)
         {
+            txtCustomerName.Clear();
+            txtGallons.Clear();
+            lstOutput.Items.Clear();
+            txtCustomerName.Focus();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
 
         }
     }
